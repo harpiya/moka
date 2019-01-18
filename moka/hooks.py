89b6deedae13e9ef219ad4f15331a0574c2b0146
harpiya <yasir@harpiya.com>
2019-01-19 01:33:51 +0300

    ups hook

--- moka/hooks.py
+++ moka/hooks.py
@@ -1,34 +1,46 @@
+# @Author: Saadettin Yasir AKEL <developer>
+# @Date:   2019-01-18T21:16:35+03:00
+# @Email:  yasir@harpiya.com
+# @Project: Harpiya Kurumsal Yönetim Sistemi
+# @Filename: hooks.py
+# @Last modified by:   developer
+# @Last modified time: 2019-01-19T01:33:38+03:00
+# @License: MIT License. See license.txt
+# @Copyright: Harpiya Yazılım Teknolojileri
+
+
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from . import __version__ as app_version
 
-app_name = "authorizenet"
-app_title = "AuthorizeNet"
-app_publisher = "DigiThinkIT, Inc."
-app_description = "Authorize.Net gateway integration"
+app_name = "moka"
+app_title = "Moka"
+app_publisher = "Harpiya Yazılım Teknolojileri."
+app_description = "Moka Ödeme Sistemi"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "forellana@digithinkit.com"
+app_email = "info@harpiya.com"
 app_license = "MIT"
 
 
-integration_services = ["AuthorizeNet"]
-app_include_js = "/assets/js/authorizenet_settings.js"
+integration_services = ["Moka"]
+app_include_js = "/assets/js/moka_settings.js"
 
 website_route_rules = [
-	{ "from_route": "/integrations/authorizenet_checkout/<name>", "to_route": "integrations/authorizenet_checkout" }
+	{ "from_route": "/integrations/moka_checkout/<name>", "to_route": "integrations/moka_checkout" }
 ]
 
 # Includes in <head>
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/authorizenet/css/authorizenet.css"
-# app_include_js = "/assets/authorizenet/js/authorizenet.js"
+# app_include_css = "/assets/moka/css/moka.css"
+# app_include_js = "/assets/moka/js/moka.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/authorizenet/css/authorizenet.css"
-# web_include_js = "/assets/authorizenet/js/authorizenet.js"
+# web_include_css = "/assets/moka/css/moka.css"
+# web_include_js = "/assets/moka/js/moka.js"
 
 # Home Pages
 # ----------
@@ -42,7 +54,7 @@ website_route_rules = [
 # }
 
 # Website user home page (by function)
-# get_website_user_home_page = "authorizenet.utils.get_home_page"
+# get_website_user_home_page = "moka.utils.get_home_page"
 
 # Generators
 # ----------
@@ -53,14 +65,14 @@ website_route_rules = [
 # Installation
 # ------------
 
-# before_install = "authorizenet.install.before_install"
-# after_install = "authorizenet.install.after_install"
+# before_install = "moka.install.before_install"
+# after_install = "moka.install.after_install"
 
 # Desk Notifications
 # ------------------
 # See frappe.core.notifications.get_notification_config
 
-# notification_config = "authorizenet.notifications.get_notification_config"
+# notification_config = "moka.notifications.get_notification_config"
 
 # Permissions
 # -----------
@@ -91,30 +103,30 @@ website_route_rules = [
 
 # scheduler_events = {
 # 	"all": [
-# 		"authorizenet.tasks.all"
+# 		"moka.tasks.all"
 # 	],
 # 	"daily": [
-# 		"authorizenet.tasks.daily"
+# 		"moka.tasks.daily"
 # 	],
 # 	"hourly": [
-# 		"authorizenet.tasks.hourly"
+# 		"moka.tasks.hourly"
 # 	],
 # 	"weekly": [
-# 		"authorizenet.tasks.weekly"
+# 		"moka.tasks.weekly"
 # 	]
 # 	"monthly": [
-# 		"authorizenet.tasks.monthly"
+# 		"moka.tasks.monthly"
 # 	]
 # }
 
 # Testing
 # -------
 
-# before_tests = "authorizenet.install.before_tests"
+# before_tests = "moka.install.before_tests"
 
 # Overriding Whitelisted Methods
 # ------------------------------
 #
 # override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "authorizenet.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "moka.event.get_events"
 # }
