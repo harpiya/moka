4fcd34d4fa96848e9b0c0008da4d3aa5ef56c77d
harpiya <yasir@harpiya.com>
2019-01-20 02:52:50 +0300

    upss

--- moka/moka/doctype/moka_settings/moka_settings.py
+++ moka/moka/doctype/moka_settings/moka_settings.py
@@ -4,7 +4,7 @@
 # @Project: Harpiya Kurumsal Yönetim Sistemi
 # @Filename: moka_settings.py
 # @Last modified by:   developer
-# @Last modified time: 2019-01-20T02:51:22+03:00
+# @Last modified time: 2019-01-20T02:52:44+03:00
 # @License: MIT License. See license.txt
 # @Copyright: Harpiya Yazılım Teknolojileri
 
