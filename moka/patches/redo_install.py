8cc46ceaa821f7e744219a4d6238feb8eeb43158
harpiya <yasir@harpiya.com>
2019-01-20 21:37:38 +0300

    uoss

--- moka/patches/redo_install.py
+++ moka/patches/redo_install.py
@@ -1,8 +1,20 @@
+# @Author: Saadettin Yasir AKEL <developer>
+# @Date:   2019-01-20T21:28:46+03:00
+# @Email:  yasir@harpiya.com
+# @Project: Harpiya Kurumsal Yönetim Sistemi
+# @Filename: redo_install.py
+# @Last modified by:   developer
+# @Last modified time: 2019-01-20T21:37:29+03:00
+# @License: MIT License. See license.txt
+# @Copyright: Harpiya Yazılım Teknolojileri
+
+
+
 import frappe
 
 def execute():
 	# should create payment gateway and payment gateway account
 	try:
-		frappe.get_doc("PayPal Settings").save()
+		frappe.get_doc("Moka Settings").save()
 	except frappe.MandatoryError:
 		pass
