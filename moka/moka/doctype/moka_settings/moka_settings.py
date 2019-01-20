2361157133f734f5fb7b5c2437ca4757be9e45ce
harpiya <yasir@harpiya.com>
2019-01-20 14:58:07 +0300

    ups

--- moka/moka/doctype/moka_settings/moka_settings.py
+++ moka/moka/doctype/moka_settings/moka_settings.py
@@ -4,7 +4,7 @@
 # @Project: Harpiya Kurumsal Yönetim Sistemi
 # @Filename: moka_settings.py
 # @Last modified by:   developer
-# @Last modified time: 2019-01-20T14:17:43+03:00
+# @Last modified time: 2019-01-20T14:58:02+03:00
 # @License: MIT License. See license.txt
 # @Copyright: Harpiya Yazılım Teknolojileri
 
@@ -277,7 +277,7 @@ class MokaSettings(Document):
 			request.log_action(json.dumps(result), "Debug")
 
 			# if all went well, record transaction id
-			#request.transaction_id = result.get('ResultCode')[0]
+			request.transaction_id = result.get('Data')
 			request.status = result.get('ResultCode') == "Success"
 			request.flags.ignore_permissions = 1
 
